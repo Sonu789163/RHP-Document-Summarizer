3eb3041ec6ed59fecff72bf6f72e6db091e388ad
Sonu-7891 <sonuverma789163@gmail.com>
2025-09-03 14:39:08 +0530

    button change

--- src/components/ChatPanel.tsx
+++ src/components/ChatPanel.tsx
@@ -864,7 +864,7 @@ export function DocumentPopover({
                   />
                   <button
                     onClick={handleDownload}
-                    className="z-99 absolute top-4 right-25 p-4 bg-[#3c3c3c] text-white rounded hover:bg-[#515a5a] rounded-full"
+                    className="z-99 absolute top-4 right-24 p-3 bg-[#3c3c3c] text-white rounded hover:bg-[#515a5a] rounded-full"
                   >
                     <svg
                       className="h-5 w-5 text-white"
